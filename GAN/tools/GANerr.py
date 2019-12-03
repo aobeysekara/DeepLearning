62acb9283c5979ca7782580626505209f2315abe
aobeysekara <asiri.obeysekara@gmail.com>
2019-12-03 15:15:28 +0000

    expanding the templates for discrimitator, generator, and loss

--- GAN/tools/GANerr.py
+++ GAN/tools/GANerr.py
@@ -6,15 +6,36 @@
 #
 #  {WORK IN PROGRESS}
 ###############################################################################
+from __future__ import absolute_import, division, print_function, unicode_literals
+try:
+  # %tensorflow_version only exists in Colab.
+  %tensorflow_version 2.x
+except Exception:
+  pass
+tf.__version__
 
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+import time
+import matplotlin.pyplot as plt
 
-__author__="Asiri I.B. Obeysekara"
-__copyright__ = "Copyright 2019, DeepLearning - AObeysekara"
-__license__ = "GPL"
-__version__ = "1.0.1"
-__maintainer__ = "A Obeysekara"
-__status__ = "testing"
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
+class Error_Estimator:
+    def __init__(self, real, fake):
+        self.real=real
+        self.fake=fake
+
+    def loss_discriminator(self):
+        real_loss= cross_entropy(tf.ones_like(self.real), self.real)
+        fake_loss= cross_entropy(tf.zeros_like(self.fake), self.fake)
+        total_loss = real_loss + fake_loss
+        return total_loss
+
+    def generator_loss(fake_output):
+        return cross_entropy(tf.ones_like(self.fake), self.fake)
 
 if __name__ == '__main__':
     print('this is home of the main GAN error estimator wrapper')
--- GAN/tools/GANnn.py
+++ GAN/tools/GANnn.py
@@ -7,28 +7,44 @@
 #  {WORK IN PROGRESS}
 ###############################################################################
 
-
-__author__="Asiri I.B. Obeysekara"
-__copyright__ = "Copyright 2019, DeepLearning - AObeysekara"
-__license__ = "GPL"
-__version__ = "1.0.1"
-__maintainer__ = "A Obeysekara"
-__status__ = "testing"
+from __future__ import absolute_import, division, print_function, unicode_literals
+try:
+  # %tensorflow_version only exists in Colab.
+  %tensorflow_version 2.x
+except Exception:
+  pass
+tf.__version__
 
 import os
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+import time
+import matplotlin.pyplot as plt
+
 import GANerr
 
 #this is discrimitator class
-class GAN_D():
-    def __init__(self,data):
+class GAN2D_D():
+    def __init__(self,main):
         self.main=main
 
+    def discriminator():
+        model = tf.keras.Sequential()
+        model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',input_shape=[28, 28, 1]))
+        return model
 
 #this is the generator class
-class GAN_G():
-    def __init__(self,data):
+class GAN2D_G():
+    def __init__(self,main):
         self.main=main
+
+    def generator():
+        model.tf.keras.Sequential()
+        model.add(laters.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+        model.add(layers.LeakyReLU())
+        return model
+
 
 if __name__ == '__main__':
     print('this is home of the main GAN generator + discriminator wrappers')
